0cce4f1f662e0f19030eb1409fc3a6c5bb4f2605
Syed Ahsan <68531690+Syed-Ahsan-Naqvi@users.noreply.github.com>
2025-01-24 16:38:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 import Main from "./components/Main.jsx"
 import About from "./components/About.jsx"
 import Skills from './components/Skills';
-import Education from "./components/Education"
-import Accomplishments from './components/Accomplishment';
-import Experience from "./components/Experience"
 import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
